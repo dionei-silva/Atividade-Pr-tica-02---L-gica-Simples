10628f96f92cab3090a2e6f041d9c26b4970b763
Dionei Silva <dionei.silva@gmail.com>
2022-11-30 16:56:13 -0300

    exercicio4-ok

--- exercicio4/script.js
+++ exercicio4/script.js
@@ -4,3 +4,11 @@ coloque o total em segundos destes minutos armazenados na
 
 primeira variável. Apresente no documento html a seguinte
 informação: "_ minutos equivale à _ segundos!" */
+
+let minutos = Number(window.prompt("Insira minutos: "));
+if (!minutos) {
+    document.write("Minutos invalidos");
+} else {
+    let segundos = minutos * 60;
+    document.write(`${minutos} minutos equivale a ${segundos} segundos.`);
+}
