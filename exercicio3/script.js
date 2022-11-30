4693ea0cec7dd404fb7558e5ae8532203f7bf5ac
Dionei Silva <dionei.silva@gmail.com>
2022-11-30 16:45:32 -0300

    exercicios3-ok

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -5,3 +5,23 @@ o valor da parcela. Apresente no documento html as seguintes
 informações:
 "O valor total da compra foi R$_,_"
 "Forma de pagamento: _x de R$_,_" */
+
+let totalCompra = Number(window.prompt("insira o valor total da compra: "));
+if (!totalCompra) {
+    document.write("valor total invalido");
+} else {
+    let parcelas = Number(window.prompt("Insira as parcelas"));
+    if (!parcelas) {
+        document.write("parcelas invalidas");
+    } else {
+        let valorParcela = Number(window.prompt("insira o valor da parcela: "));
+        if (!valorParcela) {
+            document.write("Valor parcela invalida");
+        } else {
+            document.write(`<p>o valor total da compra é ${totalCompra}</p>`);
+            document.write(
+                `<p>Forma de pagamento: ${parcelas}x de R$${valorParcela}</p>`
+            );
+        }
+    }
+}
